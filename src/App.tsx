ff60221884b9744811c357b7c5ab377daddac247
micke95 <milan.milicevic@htecgroup.com>
2020-12-27 17:58:00 +0100

    chore: remove setup files and add some test case

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useMnM } from '@mnm-tech/provider';
 
 function App() {
-  return <div>321</div>;
+  const { itemList } = useMnM();
+
+  const [receivedNumber, setReceivedNumber] = useState(-1);
+
+  useEffect(() => {
+    if (itemList.length) {
+      setReceivedNumber(itemList[0]);
+    }
+  }, [itemList]);
+
+  return <div>{`Received item is: ${receivedNumber}`}</div>;
 }
 
 export default App;
